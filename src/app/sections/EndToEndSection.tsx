3dc83a99ac1c2aeebdc80371ab48530a57ea3eb4
shubhanker <ankursaxena347@gmail.com>
2025-02-24 19:13:15 +0530

    coomplted

--- src/app/sections/EndToEndSection.tsx
+++ src/app/sections/EndToEndSection.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 function EndToEndSection() {
   return (
     <div id="aboutUs">
-      <div className='bg-[#F5EDDC] p-4 md:p-10 '>
-        <div className='flex flex-col space-y-8 md:space-y-10 mt-10 md:mt-20'>  
+      <div className='bg-[#F5EDDC] p-10 md:p-10 '>
+        <div className='flex flex-col space-y-4 md:space-y-10 mt-10 md:mt-20'>  
           <div className='text-2xl font-bold md:text-6xl text-[#180D51] text-start md:text-center  md:leading-[80px]'>
-            End-to-End Marketing Solutions:<br /> 
-            From Brand Awareness to Revenue {" "}<br className='md:block hidden' /> 
+            End-to-End Marketing Solutions:<br className='md:block hidden'  /> 
+            {" "}From Brand Awareness to Revenue {" "}<br className='md:block hidden' /> 
             Growth.
           </div>
           <p className='text-[#180D51] text-md text-start md:text-center md:text-2xl leading-relaxed md:leading-loose'>
